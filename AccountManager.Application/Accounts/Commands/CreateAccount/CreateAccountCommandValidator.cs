b6a2dfb2601954bb2303dba13f72b038fd522471
ecesari <ece.sarioglu@ichoosr.com>
2023-12-12 18:37:08 +0100

    added fluentvalidation and command validator

---
+++ AccountManager.Application/Accounts/Commands/CreateAccount/CreateAccountCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace AccountManager.Application.Accounts.Commands.CreateAccount
+{
+    public class CreateAccountCommandValidator : AbstractValidator<CreateAccountCommand>
+    {
+        public CreateAccountCommandValidator()
+        {
+            RuleFor(v => v.InitialCredit).GreaterThanOrEqualTo(0).WithMessage("Initial credit cannot be lowered than 0");
+            RuleFor(v => v.CustomerId).NotNull().WithMessage("Customer Id cannot be null");            
+        }
+    }
+}
