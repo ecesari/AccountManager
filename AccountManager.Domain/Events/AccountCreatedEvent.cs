c37ca69dd932969456659ce58d403ce2bc076c49
ecesari <ece.sarioglu@ichoosr.com>
2023-12-12 18:44:07 +0100

    added account created domain event

---
+++ AccountManager.Domain/Events/AccountCreatedEvent.cs
@@ -0,0 +1,17 @@
+﻿using AccountManager.Domain.Entities;
+using MediatR;
+
+namespace AccountManager.Domain.Events;
+
+public class AccountCreatedEvent : INotification
+{
+    public Account Account { get; }
+    public decimal InitialCredit { get; }
+
+    public AccountCreatedEvent(Account account, decimal initialCredit)
+    {
+        Account = account;
+        InitialCredit = initialCredit;
+    }
+}
+
