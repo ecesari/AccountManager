6caef547edabb51244421762478a718b4fe15d69
ecesari <ece.sarioglu@ichoosr.com>
2023-12-12 20:42:58 +0100

    added get customer handler test

--- AccountManager.Test/Handlers/Customers/GetDetailedCustomerInformationQueryTests.cs
+++ AccountManager.Test/Handlers/Customers/GetDetailedCustomerInformationQueryTests.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AccountManager.Application.Common.Exceptions;
+using AccountManager.Application.Customers.Query.GetDetailedCustomerInformation;
+using AccountManager.Domain.Entities;
+using AccountManager.Domain.Repository;
+using AutoMapper;
+using Moq;
 
 namespace AccountManager.Test.Handlers.Customers
 {
-    internal class GetDetailedCustomerInformationQueryTests
+    public class GetDetailedCustomerInformationQueryTests
     {
+        private readonly Mock<ICustomerRepository> repositoryMock;
+        private readonly Mock<IMapper> mapperMock;
+        private readonly GetDetailedCustomerInformationQueryHandler handler;
+
+        public GetDetailedCustomerInformationQueryTests()
+        {
+            repositoryMock = new Mock<ICustomerRepository>();
+            mapperMock = new Mock<IMapper>();
+            handler = new GetDetailedCustomerInformationQueryHandler(repositoryMock.Object, mapperMock.Object);
+        }
+
+        [Fact]
+        public void ConfigurationTest()
+        {
+            Assert.NotNull(handler);
+        }
+
+        [Fact]
+        public void Given_CustomerNotFound_Should_ThrowError()
+        {
+            var command = new GetDetailedCustomerInformationQuery { CustomerId = Guid.NewGuid() };
+            repositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).Returns<Customer?>(null);
+            Assert.ThrowsAsync<EntityNotFoundException>(() => handler.Handle(command, CancellationToken.None));
+        }
     }
 }
